01d2e900ec1b481c5c6a65986dbb049b45bb794b
pgs <pgs@tid.es>
2015-03-10 15:44:52 +0100

    add testInvalidCacheCollaboration and failing instantiation assertion

--- test/ResizerTest.php
+++ test/ResizerTest.php
@@ -8,14 +8,22 @@ class ResizerTest extends PHPUnit_Framework_TestCase {
     /**
      * @expectedException InvalidArgumentException
      */
+    public function testInvalidCacheCollaboration() {
+        $resizer = new Resizer('', 'nonCacheObject');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
     public function testInvalidOptionsCollaboration() {
-        $resizer = new Resizer('', 'nonOptionsObject');
+        $image = new Image('', new FileSystem(), 'nonConfigurationObject');
     }
 
     public function testInstantiation() {
         $this->assertInstanceOf('Resizer', new Resizer());
         $this->assertInstanceOf('Resizer', new Resizer(''));
-        $this->assertInstanceOf('Resizer', new Resizer('', array()));
+        $this->assertInstanceOf('Resizer', new Resizer('', new FileSystem()));
+        $this->assertInstanceOf('Resizer', new Resizer('', new FileSystem(), array()));
     }
 
     /**
