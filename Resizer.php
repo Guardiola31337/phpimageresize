d20505632da43ae2a1f85b749269af51e36e27a4
pgs <pgs@tid.es>
2015-03-10 14:11:29 +0100

    add instantiateConfiguration in order to make test pass

--- Resizer.php
+++ Resizer.php
@@ -3,16 +3,24 @@
 class Resizer {
 
     private $path;
-    private $opts;
+    private $configuration;
 
     public function __construct($path='', $opts=array()) {
         $this->path = $path;
         $this->checkOptions($opts);
-        $this->opts = $opts;
+        $this->instantiateConfiguration($opts);
     }
 
     private function checkOptions($opts) {
         if (!(is_array($opts))) throw new InvalidArgumentException();
+    }
+
+    private function instantiateConfiguration($opts) {
+        try {
+            $configuration = new Configuration($opts);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException;
+        }
     }
 
 }
