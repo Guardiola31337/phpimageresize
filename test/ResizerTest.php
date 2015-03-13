e6532a6a82343865a6d3bc707a224e412ad0bfee
Guardiola31337 <pgs@tid.es>
2015-03-13 18:21:35 +0100

    fix testComposeResizeOptions because it was failing

--- test/ResizerTest.php
+++ test/ResizerTest.php
@@ -100,6 +100,8 @@ class ResizerTest extends PHPUnit_Framework_TestCase {
     public function testComposeResizeOptions() {
         $cache = $this->getMockBuilder('FileSystem')
             ->getMock();
+        $cache->method('file_exists')
+            ->willReturn(true);
         $cache->method('getimagesize')
             ->willReturn(array('30', '20'));
 
